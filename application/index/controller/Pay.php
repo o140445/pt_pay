81ede73d270d6e0ed4fc81ad0ac08d9ed526ede8
o140445 <o140445s@gmail.com>
2024-12-14 22:11:22 +0800

    Authbank

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -5,6 +5,7 @@ namespace app\index\controller;
 use app\common\controller\Frontend;
 use app\common\model\merchant\OrderIn;
 use app\common\model\merchant\OrderRequestLog;
+use app\common\service\PaymentService;
 use fast\Http;
 use think\Cache;
 use think\Config;
@@ -60,7 +61,7 @@ class Pay extends Frontend
             return $this->view->fetch();
         }
 
-        $order = OrderIn::where('order_no', $order_id)->where('status', OrderIn::STATUS_UNPAID)->find();
+        $order = OrderIn::with('channel')->where('order_no', $order_id)->where('status', OrderIn::STATUS_UNPAID)->find();
         if (!$order) {
             $this->redirect('/404.html');
         }
@@ -69,23 +70,18 @@ class Pay extends Frontend
         $key =  'order_in_info_' . $order['order_no'];
         $response = Cache::get($key);
         if (!$response) {
-            $response = OrderRequestLog::where('order_no', $order['order_no'])->where('request_type', OrderRequestLog::REQUEST_TYPE_REQUEST)->find();
-            $response_data = json_decode($response['response_data'], true);
+            $channelService = new  PaymentService($order->channel->code);
+            $response_data = $channelService->getPayInfo($order);
         }else{
             $response_data = $response;
         }
-
-
-        // base64 qrcode转图片
-        $qrcode = "data:image/png;base64," . $response_data['qrcode'];
-        $pix_code = $response_data['copia_e_cola'];
 
         $data = [
             'order_id' => $order['order_no'],
             'amount' => 'R$ ' . number_format($order['amount'], 2, '.', ''),
             'expire_time' => date('d/m/y H:i', strtotime($order['create_time']) + 3600),
-            'qrcode'=> $qrcode,
-            'pix_code' => $pix_code,
+            'qrcode'=> $response_data['qrcode'],
+            'pix_code' => $response_data['pix_code'],
         ];
 
         // 缓存支付信息
