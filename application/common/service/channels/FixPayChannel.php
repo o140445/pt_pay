40c3cde5f44927e3d6e886f3b80b7d28af7b02f8
o140445 <o140445s@gmail.com>
2025-04-29 00:10:34 +0800

    FIX支付 本地地址

--- application/common/service/channels/FixPayChannel.php
+++ application/common/service/channels/FixPayChannel.php
@@ -3,6 +3,12 @@
 namespace app\common\service\channels;
 
 use app\common\model\merchant\OrderOut;
+use app\common\model\merchant\OrderRequestLog;
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+use Endroid\QrCode\Builder\Builder;
 use fast\Http;
 use think\Config;
 use think\Log;
@@ -68,11 +74,15 @@ class FixPayChannel implements ChannelInterface
             ];
         }
 
+        //{"status":"200","message":"success","data":{"orderStatus":"CREATED","orderMessage":"SUCCESS","merchantOrderNo":"DI20250425044802nuJKdn","platOrderNo":"Hwpay17455672854600709491484","paymentInfo":"https:\/\/pay.cxddc.top\/barzh\/Hwpay17455672854600709491484","payAmount":"5","qrcode":"00020101021226900014br.gov.bcb.pix2568qrcode.siliumpay.com.br\/dynamic\/aaf497c7-4462-474e-9b31-ecd264a5df055204000053039865802BR5904GD 36009Sao Paulo62070503***6304DC7D","description":"int","sign":"8ed7da17f604b2210a8437a1253d08c8"}}
+
+        $pay_url = Config::get('pay_url') . '/index/pay/index?order_id=' . $params['order_no'];
+
         return [
             'status' => 1, // 状态 1成功 0失败
             'pay_url' => $response['data']['paymentInfo'], // 支付地址
             'msg' => '', // 消息
-            'order_id' => $response['data']['platOrderNo'], // 订单号
+            'order_id' => $pay_url, // 订单号
             'e_no' => '',
             'request_data' => json_encode($data), // 请求数据
             'response_data' => json_encode($response), // 响应数据
@@ -236,7 +246,36 @@ class FixPayChannel implements ChannelInterface
 
     public function getPayInfo($orderIn): array
     {
-        return [];
+        $response = OrderRequestLog::where('order_no', $orderIn['order_no'])
+            ->where('request_type', OrderRequestLog::REQUEST_TYPE_REQUEST)
+            ->find();
+
+        if (!$response) {
+            throw new \Exception('支付信息获取失败！');
+        }
+
+        $responseData = json_decode($response['response_data'], true);
+//        var_dump($responseData['data']);die();
+        if (!$responseData || empty($responseData['data']['qrcode'])) {
+            throw new \Exception('二维码信息无效！');
+        }
+
+        // 配置二维码生成器
+        $renderer = new ImageRenderer(
+            new RendererStyle(200), // 二维码尺寸
+            new ImagickImageBackEnd() // 用Imagick生成图片
+        );
+
+        $writer = new Writer($renderer);
+
+        // 生成二维码图片
+        $qrCodeBinary = $writer->writeString($responseData['data']['qrcode']);
+
+        return [
+            'order_no' => $orderIn['order_no'],
+            'qrcode'   => 'data:image/png;base64,' . base64_encode($qrCodeBinary),
+            'pix_code' => $responseData['data']['qrcode'],
+        ];
     }
 
     public function getNotifyType($params): string
